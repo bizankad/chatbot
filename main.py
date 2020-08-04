656f9a62f51ecb14da178d79504c7a2334f6dfce
Brahim BIZANKAD <brhm.biz@gmail.com>
2020-08-04 22:37:50 +0200

    expose chatbot via REST api

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from chatbot.workflow import update_model, ask_bot
+from fastapi import FastAPI
+from pydantic import BaseModel
+from chatbot.workflow import update_model, ask_bot
 
-if __name__ == '__main__':
-    # database link
-    filepath = './input/chatbot_database.txt'
-    
-    # train model
-    update_model(filepath)
+# database link
+filepath = './input/chatbot_database.txt'
 
+# train model
+update_model(filepath)
+
+app = FastAPI()
+
+@app.post("/bot_answer")
+async def get_answer(question: str):
     # ask the bot
-    answer = ask_bot('Blabla')
+    answer = ask_bot(question)
 
-    print(answer)
+    return answer
