0a6dd8d6d8de25c20938f1c074b35026b60ca576
Brahim BIZANKAD <brhm.biz@gmail.com>
2020-08-05 00:24:55 +0200

    add front end

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import requests 
 import json
 
@@ -9,10 +9,22 @@ app = Flask(__name__)
 chatbot_url = 'http://127.0.0.1:8000'
 end_point = '/bot_answer'
 
+
+message_record = []
+
 @app.route('/')
 def index():
-    result = requests.post(chatbot_url+end_point, json={"question": "What is a chatbot?"})
-    return result.json()['answer']
+    return render_template('index.html', message_record=message_record)
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        user = request.form['user']
+        question = request.form['question']
+        answer = requests.post(chatbot_url+end_point, json={"question": question})
+        message_record.append(dict(user=user, question=question, answer=answer.json()['answer']))
+    
+    return redirect('/')
 
 if __name__ == '__main__':
     app.debug = True
